9d3a975c2e1d5a4ce8ff67f5dade98be93dc2962
Ren <bitadmin@bitsavvysolutions.com>
2013-10-11 21:26:01 -0700

    Added heading info on class MazeSolver.java

---
+++ Program 2/src/MazeSolver.java
@@ -0,0 +1,26 @@
+/*	MazeGrid.java
+    The GUI for the MazeSolver class
+	Phyrun Diep
+	CS310 Fall 2013
+*/  
+
+
+import data_structures.*;
+
+public class MazeSolver {
+	
+	public MazeSolver(){}
+	
+	public void mark(){}
+	
+	public boolean move(){
+		
+		return false;
+	}
+	
+	public void reset(){
+		
+	}
+	
+
+}
