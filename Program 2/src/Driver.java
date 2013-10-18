658c84cffbac6e16fe4f4e3b7f39c8ef5426b345
Ren <bitadmin@bitsavvysolutions.com>
2013-10-17 22:31:23 -0700

    added reset() function

--- Program 2/src/Driver.java
+++ Program 2/src/Driver.java
@@ -5,11 +5,13 @@ public class Driver {
 	/**
 	 * @param args
 	 */
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		MazeGrid test = new MazeGrid(null,28);
 
 	}
+
 
 }
--- Program 2/src/MazeSolver.java
+++ Program 2/src/MazeSolver.java
@@ -5,20 +5,54 @@
 */  
 
 
+import javax.swing.JOptionPane;
+
 import data_structures.*;
 
 public class MazeSolver {
 	
-	public MazeSolver(){}
+	private MazeGrid grid;
+	private Queue<GridCell> queuCell = new Queue<GridCell>();
+	private Stack<GridCell> stackCell = new Stack<GridCell>();
 	
-	public void mark(){}
+
+	
+	public MazeSolver(int dim){
+		grid = new MazeGrid(this, dim);
+	}
+	
+	public void mark(){
+		
+		//grid.markDistance();
+
+		System.out.print("Mark button was pressed");
+		 
+		 
+	}
 	
 	public boolean move(){
 		
+		queuCell.enqueue(null);
+		//grid.isValidMove(null);
 		return false;
 	}
 	
 	public void reset(){
+		
+		// Add makeEmpty() functions for Stack and Queues
+		if(!queuCell.isEmpty()){
+			System.out.print("Reset button was pressed + queue is empty");
+		}
+		else
+			queuCell.makeEmpty();
+		
+	}
+	
+	//Beginning Main method inside MazeSolver class
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new MazeSolver(20);
 		
 	}
 	
